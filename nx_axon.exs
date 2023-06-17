8593cf18cfa6d330865df6950e45df683bf1f723
Steffen Deusch <steffen@deusch.me>
2023-06-17 22:48:22 +0200

    add script with plain axon

--- nx_axon.exs
+++ nx_axon.exs
@@ -1,9 +1,9 @@
 Mix.install([
-  {:bumblebee, github: "elixir-nx/bumblebee", ref: "23de64b1b88ed3aad266025c207f255312b80ba6"},
+  {:bumblebee, "~> 0.3.0"},
   {:nx, "~> 0.5.3"},
   {:exla, "~> 0.5.3"},
   {:axon, "~> 0.5.1"},
-  {:bandit, "~> 1.0.0-pre.6"},
+  {:bandit, "~> 1.0.0-pre.9"},
   {:plug, "~> 1.14.2"}
 ])
 
@@ -13,19 +13,29 @@ model_name = "sentence-transformers/all-MiniLM-L6-v2"
 {:ok, model_info} = Bumblebee.load_model({:hf, model_name})
 {:ok, tokenizer} = Bumblebee.load_tokenizer({:hf, model_name})
 
-serving = Bumblebee.Text.TextEmbedding.text_embedding(model_info, tokenizer)
+:persistent_term.put(:model_info, model_info)
+:persistent_term.put(:tokenizer, tokenizer)
+
+defmodule Predictor do
+  def predict(model_info, tokenizer) do
+    inputs = Bumblebee.apply_tokenizer(tokenizer, ["this is a test"])
+    _embedding = Axon.predict(model_info.model, model_info.params, inputs, compiler: EXLA)
+  end
+end
 
 defmodule MyPlug do
   def init(_), do: []
 
   def call(conn, _opts) do
-    Nx.Serving.batched_run(MyServing, ["this is a test"])
+    model_info = :persistent_term.get(:model_info)
+    tokenizer = :persistent_term.get(:tokenizer)
+    Predictor.predict(model_info, tokenizer)
+
     Plug.Conn.send_resp(conn, 200, "ok")
   end
 end
 
 {:ok, _pid} = Supervisor.start_link([
-  {Nx.Serving, serving: serving, name: MyServing, defn_options: [compiler: EXLA]},
   {Bandit, plug: MyPlug, port: System.get_env("PORT", "5001") |> String.to_integer()}
 ], strategy: :one_for_one)
 
